36c3c1d94e75ed9faf690418da43dc68308e7cda
Agituser29 <adruschak@gmail.com>
2021-04-02 00:48:22 +0300

    all project done

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,15 +27,44 @@ $(function() {
    /*Smooth scroll */
 $("[data-scroll]").on("click",function(event) { 
     event.preventDefault(); 
- 
-     var blockId = $(this).data("scroll"),  
-         blockOffset = $(blockId).offset().top;
-      
-        console.log(blockOffset);  
+     var $this=$(this),
+         blockId = $this.data("scroll"),  
+         blockOffset = $(blockId).offset().top; 
+       
+      $("#nav a").removeClass("active");
+      $this.addClass("active");
+       
         /*all good*/  
       $("html,body").animate({ 
           scrollTop: blockOffset
-      },500);
-  });
- 
-});
+      },500); 
+
+    }); 
+     
+    /*Menu nav toggle */ 
+  $("#nav_toggle").on("click",function(event) { 
+     event.preventDefault(); 
+       
+     $(this).toggleClass("active");
+     $("#nav").toggleClass("active");
+  });  
+   
+   /*Collapse*/   
+  $("[data-collapse]").on("click", function(event) { 
+    event.preventDefault();  
+     
+    var $this = $(this),
+        blockId = $this.data("collapse"); 
+       $this.toggleClass("active");  
+      
+       $(blockId).slideToggle(); 
+  });   
+     /*Slider*/ 
+  $("[data-slider]").slick({ 
+     infinite: true, 
+     fade:false, 
+     slidesToShow: 1,
+     slidesToScroll: 1
+  });    
+
+  }); 
